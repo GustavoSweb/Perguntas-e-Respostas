9fd2bbfc76c7037d570a5f1ab3a4ee8f9bd1dcb1
gustavodasilvama10@gmail.com <gustavodasilvama10@gmail.com>
2022-07-22 14:08:35 -0300

    jsj

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 const express = require("express")
 const app = express()
-const MongoClient = require('mongodb').MongoClient;
-const mongoose = require("mongoose")
+//const MongoClient = require('mongodb').MongoClient;
+//const mongoose = require("mongoose")
 const handlebars = require("express-handlebars")
 const bodyParser = require("body-parser")
 // engine 
@@ -12,7 +12,8 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 //mongosse
-mongoose.connect("mongodb+srv://gustavo:progamacao10@cluster0.3wzok.mongodb.net/?retryWrites=true&w=majority");
+
+//mongoose.connect("mongodb+srv://gustavo:progamacao10@cluster0.3wzok.mongodb.net/?retryWrites=true&w=majority");
 const user = new mongoose.Schema({ name: String})
 const MyModel = mongoose.model('Test', user);
 
@@ -21,21 +22,21 @@ const gustavo = mongoose.model('Test')
 
 app.post("/add", (req, res)=> {
   
-  new gustavo({
+ /* new gustavo({
     name: req.body.name
-  }).save().then(()=>{
+}).save().then(()=>{
     res.redirect("/")
   }).catch(()=> {
     res.send("Infelizmente acorreu um erro, por favor tente novamente.")
-  })
+  })*/
     
 })
 app.get("/", (req, res)=>{
-  gustavo.find({order: [['id', 'DESC']]}).then(function(post){
+ /* gustavo.find({order: [['id', 'DESC']]}).then(function(post){
     res.render("home", {
       post:post
     })
-  })
+  })*/
 })
 app.listen(process.env.PORT || 3000, function(){
   console.log("Servidor online")
