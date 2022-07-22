37c6c745f87063137b33d4ed979250c7a002df5b
gustavodasilvama10@gmail.com <gustavodasilvama10@gmail.com>
2022-07-22 15:02:25 -0300

    hsjs

--- index.js
+++ index.js
@@ -33,12 +33,18 @@ app.post("/add", (req, res)=> {
 })
 app.get("/", (req, res)=>{
   res.render("formulario")
-  /*
-  gustavo.find({order: [['id', 'DESC']]}).then(function(post){
+
+  
+})
+app.get("/card", (req, res)=>{
+  gustavo.find({order: [['id', 'DESC']]}).then((post)=> {
     res.render("home", {
       post:post
     })
-  }).catch((erro)=> res.send("Algo deu errado"+erro))*/
+  }).catch((erro)=> {
+    res.send("Algo deu errado"+erro)
+    
+  })
 })
 app.listen(process.env.PORT || 3000, function(){
   console.log("Servidor online")
