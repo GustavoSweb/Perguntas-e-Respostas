eb91a1ef13ae30d77bc4f5417ecab5b957143bf4
gustavodasilvama10@gmail.com <gustavodasilvama10@gmail.com>
2022-08-25 22:41:10 -0300

    v3

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
-const pro = document.querySelectorAll('.pro')
+const pro = document.querySelectorAll('.fundo')
 document.addEventListener('scroll', ()=> {
   pro.forEach(element =>{
   
     
     if(window. screen. width >= 500){
-       if (this.scrollY >= element.offsetTop-400) {
+       if (this.scrollY >= element.offsetTop-00) {
+         
       element.classList.add('opacity')
+      
     }else{
       element.classList.remove('opacity')
     }
     }else{
-    if (this.scrollY >= element.offsetTop-540) {
+      
+    if (this.scrollY >= element.offsetTop+940) {
       element.classList.add('opacity')
+      console.log(element.offsetTop+940)
     }else{
       element.classList.remove('opacity')
     }}
