159ac7d5e42c1c53c3eb643609e84c209e14cfd8
gustavodasilvama10@gmail.com <gustavodasilvama10@gmail.com>
2022-08-31 10:51:22 -0300

    v6

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express")
 const app = express()
 const path = require("path")
 const PORT = process.env.PORT || 8081
+
 app.use(express.static(path.resolve('public')))
+
 app.get("/", (req, res)=> {
   res.sendFile(__dirname+"/index.html")
 })
